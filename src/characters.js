3d2d6047f670439b91d5f8f64117ba32031e98ee
Marjorie Santos <72046467+iammarge@users.noreply.github.com>
2022-08-24 10:33:41 -0300

    atualização

--- src/characters.js
+++ src/characters.js
@@ -1,34 +1,36 @@
 import data from './data/ghibli/ghibli.js';
 let movies = data.films
-let characters = movies.reduce(function(chars, film){ 
-  const people = film.people.map(function(char){
-      char.title = film.title
+let characters = movies.reduce(function (chars, film) {
+  const people = film.people.map(function (char) {
+    char.title = film.title
 
-      return char 
-  }) ;
- return chars.concat(people)
+    return char
+  });
+  return chars.concat(people)
 
 }, []);
 
 function showCharacters(data) {
   const CharactersInfo = data.map((item) => {
     return `
-  <div class="flipCard">
-         <div class="flipCardFront">
+   
+     <div class="flipCard">
+        <div class="flipCardFront">
           <p class="charactersName"><strong>${item.name}</strong></p>
           <img src="${item.img}"class="posterCharacters">
-         </div>
-        <div class="flipCardCack">
-         <ul class="cardInfos">
-           <li><strong>${item.name}</strong></li>
-           <li><strong>Gender: </strong>${item.gender}</li>
-           <li><strong>Age: </strong>${item.age}</li>
-           <li><strong>Eye color: </strong>${item.eye_color}</li>
-           <li><strong>Hair color: </strong>${item.hair_color}</li>      
-           <li><strong>Specie: </strong>${item.specie}</li>
-         </ul>
+        </div>
+          <div class="flipCardBack">
+            <ul class="cardInfos">
+              <li><strong>${item.name}</strong></li>
+              <li><strong>Gender: </strong>${item.gender}</li>
+              <li><strong>Age: </strong>${item.age}</li>
+              <li><strong>Eye color: </strong>${item.eye_color}</li>
+              <li><strong>Hair color: </strong>${item.hair_color}</li>      
+              <li><strong>Specie: </strong>${item.specie}</li>
+           </ul>
+         
           </div>
-          </div>
+    </div>
         
   `}).join('')
 
