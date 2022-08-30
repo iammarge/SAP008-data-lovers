7723fac974f1e848e3dfafeb57e040c358e06f77
Marjorie Santos <72046467+iammarge@users.noreply.github.com>
2022-08-30 19:05:15 -0300

    resolvendo conflitos

--- src/movies.js
+++ src/movies.js
@@ -28,36 +28,6 @@ function showFilms(data) {
   document.getElementById('cards').innerHTML = filmsInfo
 }
 
-showFilms(movies);
-//ORDER BY ALPHABETIC //
-
-const selectElement = document.querySelector('#order');
-
-selectElement.addEventListener('change', (event) => {
-  const value = event.target.value
-  const orderedList = alphabeticOrderMovies(movies, value)
-  showFilms(orderedList)
-});
-
-//ORDER BY ALPHABETIC //
-
-const selectElementScore = document.querySelector('#order');
-
-selectElementScore.addEventListener('change', (event) => {
-  const value = event.target.value
-  const orderedList = scoreOrder(movies, value)
-  showFilms(orderedList)
-});
-
-
-//SEARCH FILMS//
-const searchTitles = document.getElementById("txtSearch");
-function filtroPesquisa(event) {
-  const filmsByTitle = searchTitle(movies, event.target.value);
-  showFilms(filmsByTitle);
-}
-searchTitles.addEventListener("keyup", filtroPesquisa);
-
 //ORDER BY ALPHABETIC //
 
 const selectElement = document.querySelector('.titleFilms');
