d2b2fc9ee2c68fe3cf56d9fc267c462478634b3c
asmih <michelledearaujo2@gmail.com>
2022-08-30 18:51:28 -0300

    ordenar correcao

--- src/data.js
+++ src/data.js
@@ -1,29 +1,3 @@
-//SORT//
-// SORT CHARACTERS//
-  // export const alphabeticOrderChars = (data, order) => {
-  //  if (order === "asc_name") {
-    // return data.sort((a,b) => {
-      // const nameA = a.name;
-       // const nameB = b.name;
-      // if (nameA < nameB) {
-       // return -1;
-      // }
-     //  if (nameA > nameB) {
-      //   return 1;
-     // }})
-    // }
-   // if (order === "desc_name") {
-     // return data.sort((a, b) => {
-    //  const nameA = a.name;
-     // const nameB = b.name;
-     // if (nameA > nameB) {
-       // return -1;
-     // }
-     // if (nameA < nameB) {
-       // return 1;
-    //  }})
-   // }
- // };
 
 const filterDirectors = (data, value) => {
   data.filter((films) => {
@@ -33,36 +7,9 @@ const filterDirectors = (data, value) => {
 }
 
 
-//   SORT MOVIES    //
-// SORT BY A-Z / Z-A//
-//export const alphabeticOrderMovies = (data, order) => {
- // if (order === "asc_title") {
-  //return data.sort((a,b) => {
- //   const titleA = a[key];
-   // const titleB = b.title;
-   // if (titleA < titleB) {
-    //  return -1;
-   // }
-   // if (titleA > titleB) {
-    //  return 1;
-   // }})
- // }
-  // if (order === "desc_title") {
-  //  return data.sort((a, b) => {
-   // const titleA = a.title;
-   // const titleB = b.title;
-   // if (titleA > titleB) {
-   //   return -1;
-   // }
-   // if (titleA < titleB) {
-   //   return 1;
-   // }})
- // }
-// }
-//
-
-// SORT BY SCORE//
+// SORT BY KEY//
 export const orderByKey = (data, key, order) => {
+  console.log(data, key, order)
   if (order === "ascending") {
       return data.sort((a, b) => {
     if (a[key] > b[key]) {
@@ -76,10 +23,10 @@ export const orderByKey = (data, key, order) => {
 
   if (order === "descending") {
       return data.sort((a, b) => {
-    if (a[key] > b[key]) {
+    if (a[key] < b[key]) {
       return 1;
     }
-    if (a[key] < b[key]) {
+    if (a[key] > b[key]) {
       return -1;
     }
     return 0;
